9d45632ad7aab9b6d30eedb2bdfa6be3c0ce15fb
PiotrK <piotrek380@gmail.com>
2022-11-13 17:42:26 +0100

    Add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,35 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Give Feedback</h1>
+        <ul className="app-buttons">
+          <li>
+            <button type="button">good</button>
+          </li>
+          <li>
+            <button type="button">neutral</button>
+          </li>
+          <li>
+            <button type="button">bad</button>
+          </li>
+        </ul>
       </header>
+      <main>
+        <h2 className="statistics-header">Statistics</h2>
+        <ul className="statistics-list">
+          <li>
+            <span>GOOD</span>
+            <span>0</span>
+          </li>
+          <li>
+            <span>NEUTRAL</span>
+            <span>0</span>
+          </li>
+          <li>
+            <span>BAD</span>
+            <span>0</span>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
