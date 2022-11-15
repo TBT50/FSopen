5538137f7b06e633cf9f2b8ba4522b2d38ba2e32
PiotrK <piotrek380@gmail.com>
2022-11-15 12:13:07 +0100

    Anecdotes - complete task

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
-import "./App.css";
+import { useState } from "react";
+import anecdotesList from "./anecdotes";
 
-function App() {
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState(anecdotesList);
+  const [index, setIndex] = useState(0);
+  const { id, text, votes } = anecdotes[index];
+
+  const checkNumber = (index: number) => {
+    if (index > anecdotes.length - 1) {
+      return 0;
+    }
+    return index;
+  };
+
+  const nextAnecdote = () => {
+    setIndex(() => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const addVote = (id: number, text: string, votes: number) => {
+    const newAnecdote = {
+      id: id,
+      text: text,
+      votes: votes + 1,
+    };
+
+    const newAnecdotes = [...anecdotes];
+    const findIndex = newAnecdotes.findIndex((items) => items.id === id);
+    newAnecdotes[findIndex] = newAnecdote;
+    setAnecdotes(newAnecdotes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>MASTER</h1>
-      </header>
+    <div>
+      <button type="button" onClick={nextAnecdote}>
+        Next anecdote
+      </button>
+      <p>ID: {id}</p>
+      <p> {text}</p>
+      <button onClick={() => addVote(id, text, votes)}>Add vote</button>
+      <p>Number of vots: {votes}</p>
     </div>
   );
-}
+};
 
 export default App;
