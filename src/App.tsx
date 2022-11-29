02d86c90bc1e69ecd4c34005b8da8a18d0d4c451
PiotrK <piotrek380@gmail.com>
2022-11-29 18:49:07 +0100

    React - event handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import "./App.css";
 
+const Button = ({ onClick, children }: any) => {
+  return (
+    <button type="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+const PlayButton = () => {
+  const handlePlayMovie = () => {
+    alert("Movie is playing...");
+  };
+  return <Button onClick={handlePlayMovie}>Play Movie</Button>;
+};
+
+const UploadButton = () => {
+  const handleUploadMovie = () => {
+    alert("Uploading...");
+  };
+  return <Button onClick={handleUploadMovie}>Upload Movie</Button>;
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>MASTER</h1>
+        <PlayButton />
+        <UploadButton />
       </header>
     </div>
   );
