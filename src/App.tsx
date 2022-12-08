fcd79c1074148b6f66efe33754b4a646d28504cd
PiotrK <piotrek380@gmail.com>
2022-12-08 20:57:16 +0100

    Render notes with the 'map' method

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
 import "./App.css";
 
-const Button = ({ onClick, children }: any) => {
-  return (
-    <button type="button" onClick={onClick}>
-      {children}
-    </button>
-  );
-};
-
-const PlayButton = () => {
-  const handlePlayMovie = () => {
-    alert("Movie is playing...");
-  };
-  return <Button onClick={handlePlayMovie}>Play Movie</Button>;
-};
-
-const UploadButton = () => {
-  const handleUploadMovie = () => {
-    alert("Uploading...");
-  };
-  return <Button onClick={handleUploadMovie}>Upload Movie</Button>;
-};
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>MASTER</h1>
-        <PlayButton />
-        <UploadButton />
+        <h1>NOTES</h1>
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id}>{note.content}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
