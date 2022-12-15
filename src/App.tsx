fd37087ffc8a1e7b009375ed1a894e6c4a758838
PiotrK <piotrek380@gmail.com>
2022-12-15 21:26:06 +0100

    Add more courses

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,32 @@
 import "./App.css";
 
-const course = {
-  id: 1,
-  name: "Half Stack application development",
-  parts: [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-      id: 1,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-      id: 2,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-      id: 3,
-    },
-    {
-      name: "Redux",
-      exercises: 11,
-      id: 4,
-    },
-  ],
-};
+import { courses } from "./courses";
+
 function App() {
-  const sumOfExercises = course.parts.reduce(
-    (acc, value) => acc + value.exercises,
-    0
-  );
+  const sumExercises = (parts: any) => {
+    return parts.reduce((acc: any, value: any) => acc + value.exercises, 0);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{course.name}</h1>
-        <ul>
-          {course.parts.map((part) => {
-            return (
-              <li key={part.id}>
-                {part.name}: <span>{part.exercises}</span>
-              </li>
-            );
-          })}
-        </ul>
-        <p>
-          <strong>Total of {sumOfExercises} exercises</strong>
-        </p>
+        {courses.map((course) => {
+          return (
+            <>
+              <h2>{course.name}</h2>
+              <ul>
+                {course.parts.map((part) => {
+                  return (
+                    <li key={part.id}>
+                      {part.name}: <span>{part.exercises}</span>
+                    </li>
+                  );
+                })}
+              </ul>
+              <p>Total of exercises: {sumExercises(course.parts)}</p>
+            </>
+          );
+        })}
       </header>
     </div>
   );
