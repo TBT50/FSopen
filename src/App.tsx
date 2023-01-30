187aa81ffb835578e9defc421b84d79f301eab88
PiotrK <piotrek380@gmail.com>
2023-01-30 22:45:32 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
 import { useState } from "react";
+// import { PersonsList } from "./components/PersonsList";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+
+  const isPersonExist = (name: string) => {
+    return persons.some((person) => person.name === name);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewName(event.target.value);
+  };
+
+  const handleAddPerson = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (isPersonExist(newName)) {
+      alert(`${newName} ALREADY EXISTS`);
+      setNewName("");
+    } else {
+      const newPerson = {
+        id: persons.length + 1,
+        name: newName,
+      };
+      setPersons([...persons, newPerson]);
+      setNewName("");
+    }
+  };
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
+      <h1>Phonebook</h1>
+      <form onSubmit={handleAddPerson}>
         <div>
-          name: <input />
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            onChange={handleChange}
+            value={newName}
+          />
         </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <button>Add</button>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
