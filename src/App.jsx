4d746775aa2af7239b903f0dcb37b8f07d92081d
PiotrK <piotrek380@gmail.com>
2023-02-04 22:59:30 +0100

    Added a new feature - Adding notes

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,60 @@ import "./App.css";
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [newNoteValue, setNewNoteValue] = useState("");
 
-  useEffect(() => {
+  const fetchNotes = () => {
     fetch("http://localhost:3001/notes")
       .then((response) => response.json())
       .then((data) => setNotes(data));
-  }, []);
+  };
 
-  if (notes.length > 0) {
-    return (
-      <div className="App">
-        <ul>
-          {notes.map((note) => (
-            <li key={note.id}>{note.content}</li>
-          ))}
-        </ul>
+  useEffect(fetchNotes, []);
+
+  const handleAddNote = (event) => {
+    event.preventDefault();
+    const newNote = {
+      id: notes.length + 1,
+      content: newNoteValue,
+      important: true,
+    };
+    setNotes([...notes, newNote]);
+    setNewNoteValue("");
+  };
+
+  const handleNewNoteValue = (event) => {
+    setNewNoteValue(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Notes</h1>
+        <form onSubmit={handleAddNote}>
+          <div>
+            <label htmlFor="newNote">Add a new note</label>
+            <input
+              type="text"
+              id="newNote"
+              onChange={handleNewNoteValue}
+              value={newNoteValue}
+            />
+          </div>
+          <button>Add</button>
+        </form>
+
+        {notes.length > 0 ? (
+          <ul>
+            {notes.map((note) => (
+              <li key={note.id}>{note.content}</li>
+            ))}
+          </ul>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
-    );
-  }
-
-  return <div className="App">LOADING...</div>;
+    </div>
+  );
 }
 
 export default App;
